5669f4db8e70932d5b65473b0b906b1f6cc8fcfc
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-01-05 20:52:31 -0800

    added shape test js and questions for passing test

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const inquirer = require('inquirer');
-const questions = require('./lib/questions');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const setShape = require('./lib/setShape.js')
+
+// function to create new svg file using inquirer response and file system
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+}
